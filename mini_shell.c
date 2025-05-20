9152a60aa5eed30dc5652b94870e3009667cfca3
Aftys Tarik <tarikaftys123@gmail.com>
2025-05-20 21:22:43 +0100

    Update mini_shell.c

--- mini_shell.c
+++ mini_shell.c
@@ -9,14 +9,17 @@
 #define MAX_INPUT 1024
 #define MAX_ARGS 64
 
-// Fonction pour afficher le prompt
+// l'affichage
 void print_prompt() {
     printf("$> ");
     fflush(stdout);
 }
 
-// Fonction pour lire une ligne de commande
+
+
 char *read_line() {
+
+    
     char *line = malloc(MAX_INPUT);
     if (!line) {
         perror("malloc");
@@ -26,17 +29,17 @@ char *read_line() {
         free(line);
         return NULL;
     }
-    // Supprimer le saut de ligne
     size_t len = strlen(line);
     if (len > 0 && line[len - 1] == '\n')
         line[len - 1] = '\0';
     return line;
 }
-
-// Fonction pour parser la ligne en arguments
 int parse_line(char *line, char **args, int *background) {
+
+    
     int argc = 0;
     *background = 0;
+    
     char *token = strtok(line, " ");
     while (token != NULL) {
         if (strcmp(token, "&") == 0) {
@@ -50,7 +53,6 @@ int parse_line(char *line, char **args, int *background) {
     return argc;
 }
 
-// Fonction pour exécuter une commande
 void execute_command(char **args, int background) {
     pid_t pid = fork();
     if (pid < 0) {
@@ -58,13 +60,13 @@ void execute_command(char **args, int background) {
         exit(EXIT_FAILURE);
     }
     if (pid == 0) {
-        // Processus enfant
         if (execvp(args[0], args) == -1) {
             perror("Command execution failed");
             exit(EXIT_FAILURE);
         }
     } else {
-        // Processus parent
+
+        
         if (!background) {
             waitpid(pid, NULL, 0);
         } else {
@@ -73,7 +75,6 @@ void execute_command(char **args, int background) {
     }
 }
 
-// Fonction principale
 int main() {
     char *line;
     char *args[MAX_ARGS];
@@ -87,19 +88,17 @@ int main() {
             break;
         }
 
-        // Ne rien faire si ligne vide
+
         if (strlen(line) == 0) {
             free(line);
             continue;
         }
-
-        // Quitter le shell
         if (strcmp(line, "exit") == 0) {
             free(line);
             break;
         }
 
-        // Commande "cd"
+        // cd
         if (strncmp(line, "cd ", 3) == 0) {
             char *path = line + 3;
             if (chdir(path) != 0) {
@@ -108,11 +107,11 @@ int main() {
             free(line);
             continue;
         }
-
         parse_line(line, args, &background);
         execute_command(args, background);
         free(line);
     }
-
     return 0;
+
+    
 }
